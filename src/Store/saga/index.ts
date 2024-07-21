e9b5ba28b0eeb0a96cfdc1bbf66c92054e1395de
Tushar Borchate <srborchate@gmail.com>
2024-07-21 22:33:58 +0530

    Delete src/Store directory

--- src/Store/saga/index.ts
+++
@@ -1,7 +0,0 @@
-import { userSaga } from './userSaga';
-import { taskSaga } from './taskSaga';
-import { all } from 'redux-saga/effects';
-
-export function* rootSaga() {
-  yield all([userSaga(), taskSaga()]);
-}
